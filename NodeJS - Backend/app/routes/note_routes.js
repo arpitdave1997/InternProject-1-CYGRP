b4c211f46dd2111594f9b5ac5f1f56c22b4177f8
Arpit Dave <arpitdave1997>
2019-02-19 01:19:46 +0530

    API calls (Error resolved for GET (Updated))

---
+++ NodeJS - Backend/app/routes/note_routes.js
@@ -0,0 +1,51 @@
+var ObjectID = require('mongodb').ObjectID
+
+module.exports = function(app, db) {
+
+  app.get("/notes", (req, res) => {
+      db.collection('notes').find({}).toArray((err, result) => {
+          if(err) {
+              return res.status(500).send(err);
+          }
+          res.send(result);
+      });
+  });
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id) };
+    const note = { title: req.body.LoginID, title: req.body.Name, title: req.body.Password };
+    db.collection('notes').update(details, note, (err, item) => {
+      if (err) {
+        res.send({'Error':'An error has occured in PUT.'})
+      } else {
+        res.send(item)
+      };
+    });
+  });
+
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id
+    const details = {'_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err,item) => {
+      if (err) {
+        res.send({'Error':'An error has occured in DELETE.'})
+      } else {
+        res.send("Note" + id + "deleted");
+      };
+    });
+  });
+
+  app.post('/notes', (req,res) => {
+    const note = { title: req.body.LoginID, title: req.body.Name, title: req.body.Password };
+    db.collection('notes').insert(note, (err, result) => {
+        if (err) {
+          res.send({'Error' : 'An error has occured in POST.'});
+        }
+        else {
+          res.send(result.ops[0])
+        }
+    });
+  });
+};
